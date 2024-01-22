51f7d6bd3c981d3f28933803eca857789d3df643
Sbike <leroyhubert@yahoo.fr>
2024-01-22 14:23:47 +0100

    adding setup.py to init the project ( plus the .idea directory)

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='quiz_game-hubert_leroy-cdof1',
+version='1.0',
+author='Hubert Leroy',
+license='MIT',
+long_description=open('README.md').read(),
+)
